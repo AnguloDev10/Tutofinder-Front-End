4745ad9a9bd24e152e567fd3ee71bbfa2f9a4456
William <wmavalle@gmail.com>
2021-06-08 20:15:05 -0500

    "login works"

---
+++ tutofast/src/app/dashboard-tutor/dashboard-tutor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-tutor',
+  templateUrl: './dashboard-tutor.component.html',
+  styleUrls: ['./dashboard-tutor.component.css']
+})
+export class DashboardTutorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ tutofast/src/app/main.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './main.component.html',
+
+})
+export class Main implements  OnInit{
+
+  constructor() {
+
+  }
+  ngOnInit(): void{
+  }
+
+  //title = 'tutofast';
+}
