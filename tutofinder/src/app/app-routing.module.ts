807857ed3ee620a63f0ac32dc69eaeba03ccdbc5
William <wmavalle@gmail.com>
2021-06-09 20:44:18 -0500

    routes

--- tutofinder/src/app/app-routing.module.ts
+++ tutofinder/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './core/authentication/authentication.component';
+import { DashboardStudentComponent} from './core/dashboard-student/dashboard-student.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'authentication', pathMatch: 'full' },
   { path: 'authentication', component: AuthenticationComponent },
+  { path: 'dashboardStudent', component: DashboardStudentComponent}
 ];
 
 @NgModule({
