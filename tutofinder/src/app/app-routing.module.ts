4609735c62ee3a7cc5c7341f96b27f41d4cd1446
William Angulo <wmavalle@gmail.com>
2021-06-11 01:40:03 -0500

    login

--- tutofinder/src/app/app-routing.module.ts
+++ tutofinder/src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationComponent } from './core/authentication/authentication.component';
+import { LoginComponent} from './core/login/login.component';
+//import {  } from './core/authentication/authentication.component';
 import { DashboardStudentComponent } from './core/dashboard-student/dashboard-student.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent },
-  {
-    path: 'authentication',
-    component: AuthenticationComponent,
+ /* {
+    path: 'login',
+    component: LoginComponent,
     data: { appbar: false },
   },
-  { path: 'dashboardStudent', component: DashboardStudentComponent },
+  { path: 'dashboardStudent', component: DashboardStudentComponent },*/
 ];
 
 @NgModule({
--- tutofinder/src/app/app.module.ts
+++ tutofinder/src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './core/home/home.component';
-import { AuthenticationComponent } from './core/authentication/authentication.component';
-import { LoginComponent } from './core/authentication/login/login.component';
-import { RegisterComponent } from './core/authentication/register/register.component';
+import { LoginComponent } from './core/login/login.component';
 import { AngularMaterialModule } from './core/material/material.module';
 import { NotificationComponent } from './shared/notification/notification.component';
 import { AppbarComponent } from './shared/appbar/appbar.component';
@@ -19,9 +17,7 @@ import { DashboardStudentComponent } from './core/dashboard-student/dashboard-st
   declarations: [
     AppComponent,
     HomeComponent,
-    AuthenticationComponent,
     LoginComponent,
-    RegisterComponent,
     NotificationComponent,
     AppbarComponent,
     MenuItemComponent,
