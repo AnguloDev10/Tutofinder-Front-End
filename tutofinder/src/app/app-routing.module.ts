59700c750c52a20690b352f593a07d19e7759683
William <wmavalle@gmail.com>
2021-06-11 13:12:22 -0500

    login-50%

--- tutofinder/src/app/app-routing.module.ts
+++ tutofinder/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent} from './core/login/login.component';
-//import {  } from './core/authentication/authentication.component';
 import { DashboardStudentComponent } from './core/dashboard-student/dashboard-student.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, data: { appbar: false }},
+  { path: 'dashboardStudent', component: DashboardStudentComponent},
   { path: 'home', component: HomeComponent },
  /* {
     path: 'login',
--- tutofinder/src/app/core/login/login.component.ts
+++ tutofinder/src/app/core/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
+  hide1 = true;
+  username = 'student@gmail.com';
+  name = 'Jhon Student';
+  password = 'demopassword';
+  companyId = null;
+  userId = null;
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl('student@gmail.com'),
+    password: new FormControl('demopassword'),
+  });
   background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))';
   image =
     'https://idc.brightspotcdn.com/dims4/default/387d311/2147483647/resize/800x%3E/quality/90/?url=https%3A%2F%2Fidc.brightspotcdn.com%2Fc2%2Fdf%2F657f01ef4114b74d063b2618b68b%2Fadobestock-336831758.jpeg';
@@ -15,5 +26,15 @@ export class LoginComponent implements OnInit {
   }
   myStyle(): object {
     return { 'background-image': `${this.background + ',url(' + this.image}` };
+  }
+  login() {
+    // localStorage.setItem('firstName', this.name);
+    this.router.navigateByUrl('/dashboardStudent');
+  }
+  getUsername(event: any) { // without type info
+    this.username = event.target.value;
+  }
+  getPassword(event: any) { // without type info
+    this.password = event.target.value;
   }
 }
